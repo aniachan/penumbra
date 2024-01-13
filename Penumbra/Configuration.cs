314782d91417f69e7adc9f1b006396898408c7a9
aniachan <anialothaire@gmail.com>
2024-01-13 19:40:17 +0100

    Implemented Image Previews
    This commit implements image previews in Penumbra (now updated for Dawntrail)
    
    Initial update for dawntrail patch
    
    [CI] Updating repo.json for 1.3.2.100

--- Penumbra/Configuration.cs
+++ Penumbra/Configuration.cs
@@ -111,6 +111,9 @@ public class Configuration : IPluginConfiguration, ISavable, IService
     public bool   EditRawTileTransforms   { get; set; } = false;
     public bool   HdrRenderTargets        { get; set; } = true;
 
+    public bool   ShowPreviewImages         { get; set; } = true;
+    public bool   ImportPreviewImages       { get; set; } = true;
+
     public Dictionary<ColorId, uint> Colors { get; set; }
         = Enum.GetValues<ColorId>().ToDictionary(c => c, c => c.Data().DefaultColor);
 
